ae7abeceb4cb88dc24e2526775f7c25b792ad728
Mervyn0509 <mervynchew0095@gmail.com>
2024-12-30 03:38:54 +0800

    Mervyn arrange 2

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import Navbar from '../components/navbar.jsx';
+import React from "react";
+import Navbar from "../components/navbar.jsx";
 
 function App() {
   return (
     <>
-          <Navbar />
-      
+      <Navbar />
+
       <div className="section">
-      <section id="homepage">
-                <h1>section1</h1>
-          
-              </section>
-  
-        
-              <section id="contact">
-                <h1>section2</h1>
-              </section>
-      
-              <section id="aboutus">
-                <h1>section3</h1>
-              </section> 
+        <section id="homepage">
+          <h1>section1</h1>
+        </section>
+
+        <section id="contact">
+          <h1>section2</h1>
+        </section>
+
+        <section id="aboutus">
+          <h1>section3</h1>
+        </section>
       </div>
     </>
-
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,48 +1,45 @@
-import '../css/menu.css'
+import "../css/menu.css";
 import { IoMenu } from "react-icons/io5";
-import { useState } from 'react';
+import { useState } from "react";
 
 const Navbar = () => {
-    const[isOpen,setIsOpen]=useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu=()=>{
-        setIsOpen(!isOpen);
-    }
-    return ( 
-
-        
-        <>
-        
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
       <header>
-        
-          <div className="container">
-            <nav>
+        <div className="container">
+          <nav>
             <div className="logo">
-                <h2>Penang Holiao</h2>
+              Penang Holiao
             </div>
 
-           <ul className={isOpen ? "nav-link active":"nav-link"}>
-           <li><a href="#homepage" className="active">Home</a></li>
-             <li><a href="#contact">Contact Us</a></li>
-            <li><a href="#aboutus">About Us</a></li>
-
-        </ul>
-        <div className="icon" onClick={toggleMenu}>
-        <IoMenu />
+            <ul className={isOpen ? "nav-link active" : "nav-link"}>
+              <li>
+                <a href="#homepage" className="active">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#contact">Contact Us</a>
+              </li>
+              <li>
+                <a href="#aboutus">About Us</a>
+              </li>
+            </ul>
+            <div className="icon" onClick={toggleMenu}>
+              <IoMenu />
+            </div>
+          </nav>
         </div>
-        </nav>
-        </div>
-        </header>
+      </header>
 
-<div className="header2">
-    
-</div>
-
-
-
-        </>
-    );
-}
+      <div className="header2"></div>
+    </>
+  );
+};
 
 export default Navbar;
-
