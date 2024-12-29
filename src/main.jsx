14e5a77888a3e2e2ae473f8d78e7d78317d54918
Mervyn0509 <mervynchew0095@gmail.com>
2024-12-30 01:45:05 +0800

    Mervyn solve page issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+import Homepage from './homepage'
 
-createRoot(document.getElementById('root')).render(
+/*createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)*/
+
+
+// Me add
+
+const router = createBrowserRouter([
+  {
+    path: "homepage",
+    element: <Homepage/>,
+  },
+  {
+    path: "App",
+    element: <App/>,
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
